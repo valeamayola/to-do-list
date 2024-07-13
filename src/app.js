96fbcbd435f8fea73ac6d0db4006dbe11d6461d6
valentinnacode <valeamayola.contac@gmail.com>
2024-07-12 21:42:05 -0300

    add task CRUD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
+import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express()
 
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api', authRoutes);
+app.use('/api', tasksRoutes);
+
 
 export default app;
