a48fedd8df95e81588abb8e36b1eb370a9403331
valentinnacode <valeamayola.contac@gmail.com>
2024-07-16 18:32:01 -0300

    change links

--- client/config.js
+++ client/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:3000/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://to-do-list-server-flame.vercel.app/api";
--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import tasksRoutes from './routes/tasks.routes.js';
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://to-do-list-client-alpha.vercel.app/',
     credentials: true
 }));
 app.use(morgan('dev'));
