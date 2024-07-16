ce3971b300216d1d83020c68d23c8bf1ae24bbc5
valentinnacode <valeamayola.contac@gmail.com>
2024-07-16 18:24:05 -0300

    add link vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://to-do-list-server-flame.vercel.app/api',
     withCredentials: true
 });
 
